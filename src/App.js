12adf679554854c1c03e775ec02c057a83eba489
ijohansson <ijohansson@groupon.com>
2024-01-02 16:19:27 +0000

    Add title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import BillTable from "./components/BillTable.tsx";
-import Typography from "@mui/material";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div style={{ maxWidth: "600px", margin: "30px auto" }}>
-      <Typography variant="h1" gutterBottom>
+      <Typography variant="h3" gutterBottom>
       Propylon Technical Assessment
       </Typography>
       <BillTable />

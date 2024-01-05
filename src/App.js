da312f1dc9f4f5917b5aeb686681ec99d75e5953
ijohansson <ijohansson@groupon.com>
2024-01-05 16:44:04 +0000

    style: Center align header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Typography } from "@mui/material";
 const App = () => {
   return (
     <div style={{ maxWidth: "600px", margin: "30px auto" }}>
-      <Typography variant="h3" gutterBottom>
+      <Typography variant="h4" align={"center"} gutterBottom>
         Propylon Technical Assessment
       </Typography>
       <BillTable />
